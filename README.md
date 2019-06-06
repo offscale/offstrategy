574c49d1e675a6e2566b15b3c62fba4a96d03dbf
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2019-06-06 16:24:03 +1000

    [LICENSE-APACHE,LICENSE-MIT,README.md,setup.py] Added license; [setup.py] Added description

--- README.md
+++ README.md
@@ -89,3 +89,18 @@ Default strategy in `strategy.sample.json`.
   - Build in logic around different strategies (e.g.: redundancy ratio, cheapest provider)
   - Define + validate against JSON-schema
   - Add DNS support (maybe an `offregister` job?)
+
+## License
+
+Licensed under either of
+
+- Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or <https://www.apache.org/licenses/LICENSE-2.0>)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or <https://opensource.org/licenses/MIT>)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
--- offstrategy/__main__.py
+++ offstrategy/__main__.py
@@ -1,18 +1,14 @@
 from os import path
-from functools import partial
 from argparse import ArgumentParser
 from multiprocessing import Pool
 
 from __init__ import logger, __version__
 from Compute import Compute
 
-config_join = partial(path.join, path.dirname(__file__), 'config')
-
-
 def _build_parser():
     parser = ArgumentParser(description='Create compute nodes')
     parser.add_argument('-s', '--strategy', help='strategy file [strategy.sample.json]',
-                        default=config_join('strategy.sample.json'))
+                        default=path.join(path.dirname(__file__), 'config', 'strategy.sample.json'))
     parser.add_argument('-n', '--number_of_nodes', help='number of nodes to create [1]',
                         default=1, type=int)
     parser.add_argument('--provider', help='Try this provider first')
